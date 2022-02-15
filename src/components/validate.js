c7ea47992cec0e7c6df7e1ea64b173583686897f
AvaveryU <juliya.belka.19@yandex.ru>
2022-02-15 23:35:28 +0300

    validation

--- src/components/validate.js
+++ src/components/validate.js
@@ -1,47 +1,54 @@
-const showInputError = (inputElement, errorMessage) => {
-    const errorMessageElement = inputElement.closest('.form__edit').querySelector('.form__item_type_error');
-    console.log(errorMessageElement);
-    errorMessageElement.textContent = errorMessage;
-    errorMessageElement.classList.add("form__item_type_active");
-    inputElement.classList.add("form__item_type_error");
+//объект настроек
+const validationConfig = {
+  formSelector: '.form',
+  inputSelector: '.form__item',
+  errorClass: 'form__item_type_active',
+  inputErrorClass:'form__input-error',
 }
-const hideInputError = (inputElement) => {
-    inputElement.classList.remove("form__item_type_active");
-}
-
-const checkImputValidity = (inputElement) => {
-    const isInputValid = inputElement.validity.valid
-    if (!isInputValid){
-        const errorMessage = inputElement.validationMessage;
-        showInputError(inputElement, errorMessage)
-    } else {
-        hideInputError(inputElement)
-    }
-}
-
-const setEventLiseners = (formElement) => {
-  const inputList = formElement.querySelectorAll(".form__item");
+//функця валидации полей - показать ошибку
+const showInputError = (inputElement, errorElement, errorMessage, config) => {
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(config.errorClass);
+  inputElement.classList.add(config.inputErrorClass);
+};
+//функця валидации полей - скрыть ошибку
+const hideInputError = (inputElement, errorElement, config) => {
+  errorElement.textContent = '';
+  errorElement.classList.remove(config.errorClass);
+  inputElement.classList.remove(config.inputErrorClass);
+};
+//функция проверки валидности поля
+const isValid = (formElement, inputElement, config) => {
+  const errorElement = formElement.querySelector(`.form__item_type_error-${inputElement.id}`);
+  const isInputValid = inputElement.validity.valid;
+  if (!isInputValid) {
+    const errorMessage = inputElement.validationMessage;
+    showInputError(inputElement, errorElement, errorMessage, config);
+  } else {
+    hideInputError(inputElement, errorElement, config);
+  }
+};
+//добавление полям формы обработчиков
+const setEventListeners = (formElement, config) => {
+  const inputList = formElement.querySelectorAll(config.inputSelector);
   const inputListArray = Array.from(inputList);
-  const submitButtonElement = formElement.querySelector(".form__button");
+  //const submitButtonElement = formElement.querySelector(".form__button");
   inputListArray.forEach(inputElement => {
-    inputElement.addEventListener("input", (event) => {
-      const {value} = event.target;
-      checkImputValidity(inputElement);
+    inputElement.addEventListener("input", () => {
+      isValid(formElement, inputElement, config);
     });
-    
   });
 };
-
-const enableValidation = () => {
-  const formList = document.querySelectorAll(".form");
+// включение валидации 
+const enableValidation = (config) => {
+  const formList = document.querySelectorAll(config.formSelector);
   const formListArray = Array.from(formList);
   const handFormSumit = (event) => {
     event.preventDefault();
   };
-
   formListArray.forEach(formElement => {
     formElement.addEventListener("submit", handFormSumit);
-    setEventLiseners(formElement)
+    setEventListeners(formElement, config);
   });
 };
-enableValidation();
+enableValidation(validationConfig);
