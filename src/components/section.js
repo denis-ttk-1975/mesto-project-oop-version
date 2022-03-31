d7a2e96720e8a8a13849444327a60999760e79e8
Denis Ule <denis.ttk@gmail.com>
2022-03-31 22:16:18 +0300

    section from Oleg added

---
+++ src/components/section.js
@@ -0,0 +1,30 @@
+export class Section {
+  constructor({items = [], renderer}, selector) {
+    this._itemsInfo = items
+    this.selector = selector
+    this._renderer = renderer
+    this.container = document.querySelector(selector)
+  }
+
+  //добавляет все карточки в контейнер
+  renderAll(userId) {
+    this._itemsInfo = this._itemsInfo.map(
+      (element) => this.container.append(
+        this._renderer(element, userId)))
+  }
+
+  //Добавляет дом-элемент в контейнер
+  addItem(element, userId) {
+    this.container.append(this._renderer(element, userId))
+  }
+
+  //Добавлят информацию о карточках
+  getInfo(info) {
+    this._itemsInfo = Array.from(info)
+  }
+
+  //Покажет итемы в консоль
+  showItems() {
+    console.log(this._itemsInfo)
+  }
+}
