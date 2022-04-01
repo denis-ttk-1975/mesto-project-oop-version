92b9dfa5c4a26dff27a21b5d68a9111b45026ebd
Avavery <juliya.belka.19@yandex.ru>
2022-04-01 16:31:01 +0300

    Popup class and userInfo

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,15 +1,16 @@
 export default class Popup {
   constructor(popupSelector) {
     this.popupSelector = popupSelector;
+    this.popup = document.querySelector(this.popupSelector);
   }
   //метод для открытия попапа
   open() {
-    document.querySelector(this.popupSelector).classList.add("popup_opened");
+    this.popup.classList.add("popup_opened");
     window.removeEventListener("keydown", this._handleEscClose());
   }
   //метод для закрытия попапа
   close() {
-    document.querySelector(this.popupSelector).classList.remove("popup_opened");
+    this.popup.classList.remove("popup_opened");
     window.addEventListener("keydown", this._handleEscClose());
   }
   //метод для закрытия попапа при нажатии ESC
@@ -20,9 +21,7 @@ export default class Popup {
   };
   //метод для навешивания слушателей
   setEventListeners = () => {
-    document
-      .querySelector(this.popupSelector)
-      .addEventListener("mousedown", (event) => {
+    this.popup.addEventListener("mousedown", (event) => {
         if (event.target.classList.contains("popup_opened")) {
           this.close();
         }
